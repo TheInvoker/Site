9d13999a6c61e08491b42bc64c7468d720cd5161
TheInvoker <rydsouza82@gmail.com>
2018-04-03 16:27:26 -0400

    update

--- src/widgets/body/body.js
+++ src/widgets/body/body.js
@@ -4,6 +4,7 @@ define(["utils"], function(utils) {
 
     return function() {
         var div = document.createElement("div");
+        div.innerHTML = "WORLD";
         div.classList.add("rd_body");
         document.body.appendChild(div);
 
--- src/widgets/header/header.js
+++ src/widgets/header/header.js
@@ -7,15 +7,9 @@ define(["headroom", "utils"], function(Headroom, utils) {
         div.innerHTML = "HELLO";
         div.classList.add("rd_header");
         document.body.appendChild(div);
-        
+
         var headroom  = new Headroom(div);
         headroom.init(); 
-
-        setInterval(updateHeaderPadding, 200);
-
-        function updateHeaderPadding() {
-            document.body.style.paddingTop = div.clientHeight + "px";
-        }
 
         this.getElement = function() {
             return div;
