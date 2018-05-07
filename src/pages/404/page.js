a1893bf4ebb208d2f348f9f3a7091ff91c4c92f4
TheInvoker <rydsouza82@gmail.com>
2018-05-07 16:28:15 -0400

    update

--- src/pages/404/page.js
+++ src/pages/404/page.js
@@ -5,7 +5,7 @@ define(['module', "utils"], function(module, utils) {
     return function(data) {
         var div = document.createElement("div");
         div.classList.add("rd_page_404");
-        div.innerHTML = "page not found";
+        div.innerHTML = "<div align=\"center\"><img style=\"width:50%;max-height:50%;\" src=\"/images/404-permalink.png\"/></div>";
         return div;
     };
 });
